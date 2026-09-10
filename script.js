4231aa01f86d61a3328e88e4b673016b808f113d
chitsanupongphe-crypto <chitsanupong.phe@spumail.net>
2026-09-10 20:44:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Portfolio loaded");
+console.log("Chitsanupong Portfolio Loaded");
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+    entries => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("show");
+
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.1
+    }
+);
+
+sections.forEach(section => {
+
+    observer.observe(section);
+
+});
